d3d302cce39ebb7ce3519f52e01889b7f9c4f08f
luisgcostaguedes <luisgcostaguedes@gmail.com>
2022-11-30 16:03:47 -0300

    up 1h06

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,15 @@
-let texto: string;
+import express from 'express';
+import mongoose from 'mongoose';
 
-texto = 'batatinha';
-console.log(texto);
+
+
+mongoose.connect('mongodb+srv://luisgcostaguedes:Batatinha123@testlgcg.dlehg.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        const app = express();
+        const port = 3002;
+
+        app.listen(port, () => {
+            console.log(`🚀 Server is running on http:localhost:${port}`);
+        });
+    })
+    .catch(() => console.log('Erro ao se conectar ao mongodb'));
