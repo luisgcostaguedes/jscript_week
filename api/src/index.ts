073d4880785ddda0ea5fe3d333cec75c1fa437b5
luisgcostaguedes <luisgcostaguedes@gmail.com>
2022-12-06 14:30:42 -0300

    up pos queda energia

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import {router} from './router';
 
 
 
@@ -7,9 +8,12 @@ mongoose.connect('mongodb://localhost:27017')
     .then(() => {
         const app = express();
         const port = 3001;
+        app.use(express.json());
+
+        app.use(router);
 
         app.listen(port, () => {
-            console.log(`Server ir running on http://localhost:${port}`);
+            console.log(` 🚀 Server ir running on http://localhost:${port}`);
         });
     })
     .catch(() => console.log('Erro ao conectar no mongodb'));
