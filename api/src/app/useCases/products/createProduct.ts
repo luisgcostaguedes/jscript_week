58c895466285668ea11ddd1b16da7e6c3644f49e
luisgcostaguedes <luisgcostaguedes@gmail.com>
2022-12-08 09:56:34 -0300

    up

--- api/src/app/useCases/products/createProduct.ts
+++ api/src/app/useCases/products/createProduct.ts
@@ -5,7 +5,6 @@ import { Product } from '../../models/Product';
 export async function createProduct(req: Request, res: Response) {
 
     try {
-
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
